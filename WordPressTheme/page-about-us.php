4b1d19e02ae492945e92ce3e7c3e45f3473bdb86
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-12-05 22:24:34 +0900

    gallery繰り返しフィールド適応

--- WordPressTheme/page-about-us.php
+++ WordPressTheme/page-about-us.php
@@ -46,26 +46,29 @@
       <p class="section-title__sub">Gallery</p>
       <h2 class="section-title__main">フォト</h2>
     </div>
-    <ul class="gallery__list gallery-list">
-      <li class="gallery-list__item js-modal-open" data-target="1">
-        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/gallery1.jpg" alt="珊瑚礁の周りをオレンジ色の魚達が泳いでいる様子" />
-      </li>
-      <li class="gallery-list__item js-modal-open" data-target="2">
-        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/gallery2.jpg" alt="綺麗な海に船が3隻停泊している様子" />
-      </li>
-      <li class="gallery-list__item js-modal-open" data-target="3">
-        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/gallery3.jpg" alt="2匹の熱帯魚が泳いでいる様子" />
-      </li>
-      <li class="gallery-list__item js-modal-open" data-target="4">
-        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/gallery4.jpg" alt="1匹の熱帯魚が泳いでいる様子" />
-      </li>
-      <li class="gallery-list__item js-modal-open" data-target="5">
-        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/gallery5.jpg" alt="熱帯魚が群れで泳いでいる様子" />
-      </li>
-      <li class="gallery-list__item js-modal-open" data-target="6">
-        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/gallery6.jpg" alt="珊瑚礁の周りを黄色い魚が泳いでいる様子" />
-      </li>
-    </ul>
+    <?php
+// SCFから繰り返しフィールドを取得
+$gallery_images = SCF::get('gallery_images'); // 繰り返しフィールド名 'gallery_images'
+if (!empty($gallery_images)): // 繰り返しフィールドが空でない場合
+    echo '<div class="gallery-wrapper">';
+    foreach ($gallery_images as $gallery_image): // 繰り返し処理
+        // 各サブフィールドの画像URLを取得して出力
+        for ($i = 1; $i <= 6; $i++): // gallery-image1 から gallery-image6 までループ
+            $field_name = 'gallery-image' . $i; // 動的にフィールド名を生成
+            if (!empty($gallery_image[$field_name])): // フィールドが空でないか確認
+                $image_url = wp_get_attachment_url($gallery_image[$field_name]); // 画像URLを取得
+                if ($image_url):
+                    echo '<div class="gallery-item">';
+                    echo '<img src="' . esc_url($image_url) . '" alt="ギャラリー画像">';
+                    echo '</div>';
+                endif;
+            endif;
+        endfor;
+    endforeach;
+    echo '</div>';
+endif;
+?>
+
   </div>
   <!-- モーダル -->
   <div class="gallery__modal gallery-modal">
