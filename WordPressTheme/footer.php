43976421bc278a99f431bce2dd7f0eda6a8b9710
{kurachihirofumi} <{kuratch37@gmail.com}>
2025-01-28 06:21:05 +0900

    リンク修正

--- WordPressTheme/footer.php
+++ WordPressTheme/footer.php
@@ -61,7 +61,7 @@
       <footer class="footer footer-top">
         <div class="footer__inner inner">
           <div class="footer__title footer-title">
-            <a href="#" class="footer-title__name-layout">
+            <a href="<?php echo esc_url(home_url('/')); ?>" class="footer-title__name-layout">
               <h3 class="footer-title__name">
                 <img class="footer-title__name-logo" src="<?php echo get_theme_file_uri(); ?>/assets/images/logo.svg"
                   alt="CodeUps" />
