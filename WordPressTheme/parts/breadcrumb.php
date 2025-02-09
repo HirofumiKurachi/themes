1abf34eec70f907ca23e9fa2071653717e367b98
{kurachihirofumi} <{kuratch37@gmail.com}>
2025-02-09 23:02:17 +0900

    ZOOM FB 404ページに移行したら条件分岐して404用のCSSが反映されるように修正

--- WordPressTheme/parts/breadcrumb.php
+++ WordPressTheme/parts/breadcrumb.php
@@ -1,9 +1,9 @@
-<div class="breadcrumb breadcrumb-top">
+<div class="breadcrumb <?php echo is_404() ? 'breadcrumb-404 breadcrumb-404-top' : 'breadcrumb-top'; ?>">
   <div class="breadcrumb__inner inner">
     <?php
       if (function_exists('bcn_display')) {
         bcn_display();
       }
-      ?>
+    ?>
   </div>
 </div>
